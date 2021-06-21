c9832726009a9e12e761fd846c54038739b6d835
Puneet Pugalia <pugaliapuneet@gmail.com>
2021-06-21 19:41:00 +0530

    Show previous value

--- src/components/elements/TextField/TextField.js
+++ src/components/elements/TextField/TextField.js
@@ -5,7 +5,11 @@ export default function TextField(props) {
   return (
     <div className="text-field-holder">
       <p1 className="text-field-title" style={props.titleTopMargin ? {marginTop: props.titleTopMargin} : {}}>{props.title}</p1>
-      <input type={props.textFieldType ? props.textFieldType : "text"} onChange={(event) => props.setInputValue(event.target.value)} className="text-field-form" placeholder={props.placeholder} />
+      <input type={props.textFieldType ? props.textFieldType : "text"}
+      value={props.value}
+      onChange={(event) => props.setInputValue(event.target.value)} 
+      className="text-field-form" 
+      placeholder={props.placeholder} />
     </div>
   )
 }
--- src/components/elements/TextView/TextView.js
+++ src/components/elements/TextView/TextView.js
@@ -5,7 +5,11 @@ export default function TextView(props) {
   return (
     <div className="text-view-holder">
       <p1 className="text-view-title" style={props.titleTopMargin ? {marginTop: props.titleTopMargin} : {}}>{props.title}</p1>
-      <textarea className="text-view-form" type={"text"} style={{height: props.height ? props.height : '125px'}} onChange={(event) => props.setInputValue(event.target.value)} placeholder={props.placeholder} />
+      <textarea className="text-view-form" 
+      value={props.value}
+      type={"text"} style={{height: props.height ? props.height : '125px'}} 
+      onChange={(event) => props.setInputValue(event.target.value)} 
+      placeholder={props.placeholder} />
     </div>
   )
 }
