c0aef6b0a9d3034d4cc7d2ff58c3b09b422adcc1
Aleh Buyalski <zidabah2@gmail.com>
2022-01-30 00:27:14 +0300

    - Remove unused actions that were replaced with the generic one - Update Swipe actions for "right" and "left" handlers - Update re-swipe actions

--- src/store/category.js
+++ src/store/category.js
@@ -1,6 +1,8 @@
 import initialState, { buckets } from "./initial-state";
 import { forEach, map, union, values } from "lodash";
 import emojis from "./emojiicons";
+import { getCategorySymbolByPosition, getFirstExternalCategoryPosition } from "../utils/category";
+
 const MAX_BUCKET_SIZE = 10;
 const getAvailableLinks = (allLinks, availableLinks) => {
   forEach(availableLinks, (avLink) => {
@@ -37,17 +39,13 @@ const getProcessedCollection = (state, action, type) => {
   let goBack = action.payload.curIndex <= new Date().setUTCHours(0, 0, 0, 0)
     && action.payload.count !== 0 && !action.payload.random
 
-  // console.log(action.payload.curIndex <= new Date().setUTCHours(0, 0, 0, 0),!action.payload.random, action.payload.count !== 0)
+  let bucket;
 
-  var bucket = []
-  if(action.payload && action.payload.drops ){
+  if (action.payload && action.payload.drops) {
     bucket = !goBack ? action.payload.drops : action.payload.drops.reverse()
-  }
-  else{
+  } else {
     bucket = !goBack ? action.payload.data : action.payload.data.reverse()
   }
-
-  
 
   let pastIndex = null
 
@@ -65,7 +63,6 @@ const getProcessedCollection = (state, action, type) => {
     ...currentCollectionState,
     activeBucket: bucket
   };
-  // console.log(bucket);
 
   return {
     ...state,
@@ -78,13 +75,11 @@ const getProcessedCollection = (state, action, type) => {
 
 
 const categoryReducer = (state = initialState, action) => {
-  const tabList = ["art", "music", "collectible", "fashion","CloneX","TWV","SUPR","DOODLE","BAYC","WOW"];
-  // const tabList = ["art"];
   const loadingIndexList = [...state.general.loadingIndexList];
+
   if (action.payload?.activeTabIndex) {
     loadingIndexList.push(action.payload.activeTabIndex);
   }
-  
 
   switch (action.type) {
     case "general": {
@@ -100,175 +95,23 @@ const categoryReducer = (state = initialState, action) => {
       return { ...state , loading: true };
     }
     case "FETCH_CATEGORY_SUCCESS" : {
-      // console.log(action.payload);
-      return {...state , loading: false, allCategories : action.payload };
+      return {
+        ...state,
+        general: {
+          ...state.general,
+          activeTabIndex: getFirstExternalCategoryPosition(action.payload),
+        },
+        loading: false,
+        allCategories : action.payload
+      };
     }
 
     case "CHANGE_CUR_INDEX": {
       return { ...state, curIndex: action.payload }
     }
-    case "FETCH_MUSIC_REQUEST": {
-      const general = {
-        ...state.general,
-        activeTabIndex: action.payload.activeTabIndex,
-        isLoading: true,
-        loadingIndexList: loadingIndexList
-      };
-      return { ...state, general };
-    }
-    case "FETCH_MUSIC_SUCCESS": {
-      const musicCollection = getProcessedCollection(state, action, "music");
-      return musicCollection;
-    }
-    case "FETCH_ARTS_REQUEST": {
-      const general = {
-        ...state.general,
-        activeTabIndex: action.payload.activeTabIndex,
-        isLoading: true,
-        loadingIndexList: loadingIndexList
-      };
-      return { ...state, general };
-    }
-    case "FETCH_ARTS_SUCCESS": {
-      const artsCollection = getProcessedCollection(state, action, "art");
-      return artsCollection;
-    }
-
-
-    case "FETCH_CloneX_REQUEST": {
-      const general = {
-        ...state.general,
-        activeTabIndex: action.payload.activeTabIndex,
-        isLoading: true,
-        loadingIndexList: loadingIndexList
-      };
-      // console.log(action.payload.activeTabIndex)
-      return { ...state, general };
-    }
-    case "FETCH_CloneX_SUCCESS": {
-      const cloneXCollection = getProcessedCollection(state, action, "CloneX");
-      // console.log(cloneXCollection);
-      return cloneXCollection;
-    }
-
-    case "FETCH_WOW_REQUEST": {
-      const general = {
-        ...state.general,
-        activeTabIndex: action.payload.activeTabIndex,
-        isLoading: true,
-        loadingIndexList: loadingIndexList
-      };
-      // console.log(action.payload.activeTabIndex)
-      return { ...state, general };
-    }
-    case "FETCH_WOW_SUCCESS": {
-      const cloneXCollection = getProcessedCollection(state, action, "WOW");
-      // console.log(cloneXCollection);
-      return cloneXCollection;
-    }
-
-    case "FETCH_DOODLE_REQUEST": {
-      const general = {
-        ...state.general,
-        activeTabIndex: action.payload.activeTabIndex,
-        isLoading: true,
-        loadingIndexList: loadingIndexList
-      };
-      // console.log(action.payload.activeTabIndex)
-      return { ...state, general };
-    }
-    case "FETCH_DOODLE_SUCCESS": {
-      const cloneXCollection = getProcessedCollection(state, action, "DOODLE");
-      // console.log(cloneXCollection);
-      return cloneXCollection;
-    }
-
-    case "FETCH_BAYC_REQUEST": {
-      const general = {
-        ...state.general,
-        activeTabIndex: action.payload.activeTabIndex,
-        isLoading: true,
-        loadingIndexList: loadingIndexList
-      };
-      // console.log(action.payload.activeTabIndex)
-      return { ...state, general };
-    }
-    case "FETCH_BAYC_SUCCESS": {
-      const cloneXCollection = getProcessedCollection(state, action, "BAYC");
-      // console.log(cloneXCollection);
-      return cloneXCollection;
-    }
-
-    case "FETCH_DWolves_REQUEST": {
-      const general = {
-        ...state.general,
-        activeTabIndex: action.payload.activeTabIndex,
-        isLoading: true,
-        loadingIndexList: loadingIndexList
-      };
-      // console.log(action.payload.activeTabIndex)
-      return { ...state, general };
-    }
-    case "FETCH_DWolves_SUCCESS": {
-      const cloneXCollection = getProcessedCollection(state, action, "TWV");
-      return cloneXCollection;
-    }
-
-    case "FETCH_SuperRare_REQUEST": {
-      const general = {
-        ...state.general,
-        activeTabIndex: action.payload.activeTabIndex,
-        isLoading: true,
-        loadingIndexList: loadingIndexList
-      };
-      // console.log(action.payload.activeTabIndex)
-      return { ...state, general };
-    }
-    case "FETCH_SuperRare_SUCCESS": {
-      const cloneXCollection = getProcessedCollection(state, action, "SUPR");
-      return cloneXCollection;
-    }
 
     case "FETCH_EC_SUCCESS": {
-      // console.log('ec fetch success',action.payload)
       return {...state , EC : action.payload};
-    }
-
-
-    case "FETCH_COLLECTIBLES_REQUEST": {
-      const general = {
-        ...state.general,
-        activeTabIndex: action.payload.activeTabIndex,
-        isLoading: true,
-        loadingIndexList: loadingIndexList
-      };
-      return { ...state, general };
-    }
-    case "FETCH_COLLECTIBLES_SUCCESS": {
-      const collectiblesCollection = getProcessedCollection(
-        state,
-        action,
-        "collectible"
-      );
-      return collectiblesCollection;
-    }
-
-    case "FETCH_FASHION_REQUEST": {
-      const general = {
-        ...state.general,
-        activeTabIndex: action.payload.activeTabIndex,
-        isLoading: true,
-        loadingIndexList: loadingIndexList
-      };
-      return { ...state, general };
-    }
-    case "FETCH_FASHION_SUCCESS": {
-      const fashionCollection = getProcessedCollection(
-        state,
-        action,
-        "fashion"
-      );
-      return fashionCollection;
     }
 
     // case "FETCH_RESWIPE_REQUEST": {
@@ -292,11 +135,10 @@ const categoryReducer = (state = initialState, action) => {
     //   return { ...state, general, [currentTab]: selectedTabContent };
     // }
     case "ADD_USER_DATA": {
-      const currentTab = tabList[state.general.activeTabIndex];
-      // console.log(currentTab , state.general.activeTabIndex);
+      const currentTab = getCategorySymbolByPosition(state.general.activeTabIndex, state.allCategories);
+
       const activeTabContent = state[currentTab];
-      // console.log('bucket',currentTab)
-      // console.log('bucket',state[currentTab]);
+
       if(!activeTabContent) return {...state} ;
       const { reswipedDrops, activeBucket } = activeTabContent;
       const { drop_id, dropIndex } = action.payload;
@@ -321,16 +163,15 @@ const categoryReducer = (state = initialState, action) => {
       }
     }
     case "REMOVE_USER_DATA": {
-      const currentTab = tabList[state.general.activeTabIndex];
+      const currentTab = getCategorySymbolByPosition(state.general.activeTabIndex, state.allCategories);
       const activeTabContent = state[currentTab];
-      // console.log('active t c',activeTabContent)
+
       if(!activeTabContent) return {...state} ;
       const { reswipedDrops } = activeTabContent;
 
       const { drop_id } = action.payload;
 
       delete reswipedDrops[drop_id];
-      // console.log()
       const general = {
         ...state.general,
         selectionCount: state.general.selectionCount - 1,
@@ -348,7 +189,6 @@ const categoryReducer = (state = initialState, action) => {
       return { ...state, general };
     }
     case "FETCH_LINKS_SUCCESS": {
-      // console.log(action.payload)
       const general = { ...state.general, isLoading: false };
       return {
         ...state,
@@ -379,150 +219,49 @@ const categoryReducer = (state = initialState, action) => {
 
     case "START_RESWIPE": {
       const { newBucket } = action.payload;
-      const currentTab = tabList[state.general.activeTabIndex];
-      const reswipedDrops = {
-        "art": {},
-        "music": {},
-        "fashion": {},
-        "collectible": {},
-        "CloneX":{},
-        "TWV":{},
-        "SUPR":{},
-        "DOODLE":{},
-        "BAYC":{},
-        "WOW":{}
-      };
-      newBucket.map((d) => {
-        switch (d.category) {
-          case 'art': {
-            reswipedDrops["art"][d.id] = d;
-            break;
-          }
-          case 'music': {
-            reswipedDrops["music"][d.id] = d;
-            break;
-          }
-          case 'fashion': {
-            reswipedDrops["fashion"][d.id] = d;
 
-            break;
-          }
-          case 'collectible': {
-            reswipedDrops["collectible"][d.id] = d;
-            break;
-          }
-          case 'CloneX': {
-            reswipedDrops["CloneX"][d.id] = d;
-            break;
-          }
-          case 'TWV': {
-            reswipedDrops["TWV"][d.id] = d;
-            break;
-          }
-          case 'SUPR': {
-            reswipedDrops["SUPR"][d.id] = d;
-            break;
-          }
-          case 'DOODLE': {
-            reswipedDrops["DOODLE"][d.id] = d;
-            break;
-          }
-          case 'BAYC': {
-            reswipedDrops["BAYC"][d.id] = d;
-            break;
-          }
-          case 'WOW': {
-            reswipedDrops["WOW"][d.id] = d;
-            break;
-          }
-          default: ;
+      const reswipedDrops = {};
+      newBucket.forEach((d) => {
+        if (!reswipedDrops[d.category]) {
+          reswipedDrops[d.category] = {};
         }
 
-      })
+        reswipedDrops[d.category][d.id] = d;
+      });
+
 
       const general = {
         ...state.general,
-        reswipeModeActive: Object.keys(reswipedDrops['art']).length >= 1 ? true : false
+        reswipeModeActive: false // Object.keys(reswipedDrops['art').length >= 1
       }
+
+      const updatedCategories = Object.entries(reswipedDrops).reduce((previousValue, [key, value]) => ({
+          ...previousValue,
+          [key]: {
+            ...state[key],
+            reswipedDrops: value,
+          }
+        }), {});
+
       return {
         ...state,
         general,
-        "art": { ...state.art, reswipedDrops: reswipedDrops["art"] },
-        "collectible": { ...state.collectible, reswipedDrops: reswipedDrops["collectible"] },
-        "music": { ...state.music, reswipedDrops: reswipedDrops["music"] },
-        "fashion": { ...state.fashion, reswipedDrops: reswipedDrops["fashion"] },
-        "CloneX": { ...state.CloneX, reswipedDrops: reswipedDrops["CloneX"] },
-        "TWV": { ...state.TWV, reswipedDrops: reswipedDrops["TWV"] },
-        "SUPR": { ...state.SUPR, reswipedDrops: reswipedDrops["SUPR"] },
-        "DOODLE": { ...state.DOODLE, reswipedDrops: reswipedDrops["DOODLE"] },
-        "BAYC": { ...state.BAYC, reswipedDrops: reswipedDrops["BAYC"] },
-        "WOW": { ...state.WOW, reswipedDrops: reswipedDrops["WOW"] },
+        ...updatedCategories
       }
     }
 
     case 'SAVE_RESWIPE_BUCKETS': {
       const savedDrops = action.payload;
-      const currentTab = tabList[state.general.activeTabIndex];
-      const reswipedDrops = {
-        "art": {},
-        "music": {},
-        "fashion": {},
-        "collectible": {},
-        "CloneX":{},
-        "TWV":{},
-        "SUPR":{},
-        "DOODLE":{},
-        "BAYC":{},
-        "WOW":{}
-      };
-      savedDrops.map((d) => {
-        switch (d.category) {
-          case 'art': {
-            reswipedDrops["art"][d.id] = d;
-            break;
-          }
-          case 'music': {
-            reswipedDrops["music"][d.id] = d;
-            break;
-          }
-          case 'fashion': {
-            reswipedDrops["fashion"][d.id] = d;
+      const currentTab = getCategorySymbolByPosition(state.general.activeTabIndex, state.allCategories);
+      const reswipedDrops = {};
 
-            break;
-          }
-          case 'collectible': {
-            reswipedDrops["collectible"][d.id] = d;
-            break;
-          }
-          case 'CloneX': {
-            reswipedDrops["CloneX"][d.id] = d;
-            break;
-          }
-          case 'TWV': {
-            reswipedDrops["TWV"][d.id] = d;
-            break;
-          }
-          case 'SUPR': {
-            reswipedDrops["SUPR"][d.id] = d;
-            break;
-          }
-          
-          case 'DOODLE': {
-            reswipedDrops["DOODLE"][d.id] = d;
-            break;
-          }
-          case 'BAYC': {
-            reswipedDrops["BAYC"][d.id] = d;
-            break;
-          }
-          case 'WOW': {
-            reswipedDrops["WOW"][d.id] = d;
-            break;
-          }
-          default: ;
+      savedDrops.forEach((d) => {
+        if (!reswipedDrops[d.category]) {
+          reswipedDrops[d.category] = {};
         }
 
-      })
+        reswipedDrops[d.category][d.id] = d;
+      });
 
       let reswipeModeActive = false;
       if (Object.keys(reswipedDrops[currentTab]).length >= MAX_BUCKET_SIZE) {
@@ -532,20 +271,20 @@ const categoryReducer = (state = initialState, action) => {
         ...state.general,
         reswipeModeActive
       }
-      // console.log('here to check array');
+
+      const updatedCategories = Object.entries(reswipedDrops).reduce(
+        (previousValue, [key, value]) => ({
+          ...previousValue,
+          [key]: {
+            ...state[key],
+            reswipedDrops: value,
+          }
+        }), {});
+
       return {
         ...state,
         general,
-        "art": {...state.art, reswipedDrops: reswipedDrops["art"]},
-        "collectible": {...state.collectible, reswipedDrops: reswipedDrops["collectible"]},
-        "music": {...state.music, reswipedDrops: reswipedDrops["music"]},
-        "fashion": {...state.fashion, reswipedDrops: reswipedDrops["fashion"]},
-        "CloneX": {...state.CloneX, reswipedDrops: reswipedDrops["CloneX"]},
-        "TWV": {...state.TWV, reswipedDrops: reswipedDrops["TWV"]},
-        "SUPR": {...state.SUPR, reswipedDrops: reswipedDrops["SUPR"]},
-        "DOODLE": {...state.DOODLE, reswipedDrops: reswipedDrops["DOODLE"]},
-        "BAYC": {...state.BAYC, reswipedDrops: reswipedDrops["BAYC"]},
-        "WOW": {...state.WOW, reswipedDrops: reswipedDrops["WOW"]},
+        ...updatedCategories,
       }
     }
     case 'FETCH_MORE_FEEDS': {
